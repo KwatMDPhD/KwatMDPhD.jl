8ade90874f9e193f7b44968b71e60a0e08b5394e
KwatMDPhD <kwat.me@icloud.com>
2022-02-02 15:37:52 -0700

    svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,10 @@
+//import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+		adapter: adapter()
 	}
 };
 
