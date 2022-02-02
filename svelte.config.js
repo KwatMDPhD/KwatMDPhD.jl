8539dfce5ad2991f51761a251c344972442c60eb
KwatMDPhD <kwat.me@icloud.com>
2022-02-02 15:44:39 -0700

    svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+  },
 };
 
 export default config;
