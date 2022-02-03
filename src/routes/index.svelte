32af9f1da0ddfd12784ec1028a81a3402d485d50
KwatMDPhD <kwat.me@icloud.com>
2022-02-02 20:16:08 -0700

    Updated text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,17 @@
-<h1>Howdy!</h1>
+<h1>I'm Kwat :)</h1>
 
-<p>I'm Kwat :)</p>
+<h3>Less is more.</h3>
 
-<p>This website grows as I learn more about svelte.</p>
+<p>I'm trying to teach medicine, coding, and more on <a href="https://www.youtube.com/c/KwatMDPhD">YouTube</a>.</p>
+
+<p>I keep some code on <a href="https://github.com/KwatMDPhD">GitHub</a>.</p>
+
+<p>I used to post my thoughts on <a href="https://www.instagram.com/KwatMDPhD">Instagram</a>.</p>
+
+<p>I don't like <a href="https://twitter.com/KwatMDPhD">Twitter</a>.</p>
+
+<p>I was told to get into <a href="https://www.tiktok.com/@KwatMDPhD">TikTok</a>.</p>
+
+<p>When I complete the things I'm building, I'll share them with you on these channels ;)</p>
+
+<h3>Every adversity, carries with it, the seed of equivalent advantage.</h3>
