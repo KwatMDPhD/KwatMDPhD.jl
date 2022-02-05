21c834c6b3640112f84811d9648999e999fd67be
KwatMDPhD <kwat.me@icloud.com>
2022-02-04 20:16:06 -0700

    Links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,18 +2,24 @@
 
 <h3>Less is more.</h3>
 
-<p>I'm trying to teach medicine, coding, and more on <a href="https://www.youtube.com/c/KwatMDPhD">YouTube</a>.</p>
+<p><a href="https://github.com/KwatMDPhD">GitHub</a>.</p>
 
-<p>I keep some code on <a href="https://github.com/KwatMDPhD">GitHub</a>.</p>
+<p><a href="https://scholar.google.com/citations?user=PAM4lScAAAAJ&hl=en">Google Scholar</a>.</p>
 
-<p>I used to post my thoughts on <a href="https://www.instagram.com/KwatMDPhD">Instagram</a>.</p>
+<p><a href="https://www.youtube.com/c/KwatMDPhD">YouTube</a>.</p>
 
-<p>I don't like <a href="https://twitter.com/KwatMDPhD">Twitter</a>.</p>
+<p><a href="https://www.instagram.com/KwatMDPhD">Instagram</a>.</p>
 
-<p>I was told to get into <a href="https://www.tiktok.com/@KwatMDPhD">TikTok</a>.</p>
+<p><a href="https://twitter.com/KwatMDPhD">Twitter</a>.</p>
 
-<p>I do meetings on <a href="https://stanford.zoom.us/my/KwatMDPhD">Zoom</a> (ask for the passcode).</p>
+<p><a href="https://www.tiktok.com/@KwatMDPhD">TikTok</a>.</p>
 
-<p>When I complete the things I'm building, I'll share them with you on these channels ;)</p>
+<p><a href="https://stanford.zoom.us/my/KwatMDPhD">Zoom</a> (ask for the passcode).</p>
+
+<p>When I complete the things I'm building, I'll share them with you on these channels :)</p>
 
 <h3>Every adversity, carries with it, the seed of equivalent advantage.</h3>
+
+<p><a href="https://guardiome.com">Private whole-genome sequencing</a>.</p>
+
+<p><a href="https://www.amazon.com/s?i=stripbooks&rh=p_27%3AKwat+Medetgul-Ernar&s=relevancerank&text=Kwat+Medetgul-Ernar&ref=dp_byline_sr_book_2">Pathogen books for children</a>.</p>
