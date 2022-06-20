f848aec302f9a7093eef1c8329ffa0abdfb2a642
KwatMDPhD <kwat.me@icloud.com>
2022-06-20 12:56:23 -0600

    Cleaned

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,22 @@
-<h1>I'm Kwat :)</h1>
+<h1>Hi, I'm Kwat :)</h1>
 
-<h3>Less is more.</h3>
+<h5>Nomad from Kazakhstan → China → Japan → FIBA U16 Olympian → USA → Leukemia → Homeless → Startup → Stanford MD-PhD candidate → More startups →</h5>
 
-<p><a href="https://github.com/KwatMDPhD">GitHub</a>.</p>
+<h4>I ...</h4>
 
-<p><a href="https://scholar.google.com/citations?user=PAM4lScAAAAJ&hl=en">Google Scholar</a>.</p>
+<p>keep some code @ <a href="https://github.com/KwatMDPhD">GitHub</a>.</p>
 
-<p><a href="https://www.youtube.com/c/KwatMDPhD">YouTube</a>.</p>
+<p>list my publications @ <a href="https://scholar.google.com/citations?user=PAM4lScAAAAJ&hl=en">Google Scholar</a>.</p>
 
-<p><a href="https://www.instagram.com/KwatMDPhD">Instagram</a>.</p>
+<p>record medical podcasts (daily) @ <a href="https://kwatmdphd.sounder.fm">Sounder</a>.</p>
 
-<p><a href="https://twitter.com/KwatMDPhD">Twitter</a>.</p>
+<p>make medical videos (occasionally) @ <a href="https://www.youtube.com/c/KwatMDPhD">YouTube</a>.</p>
 
-<p><a href="https://www.tiktok.com/@KwatMDPhD">TikTok</a>.</p>
+<p>share moments @ <a href="https://www.instagram.com/KwatMDPhD">Instagram</a>.</p>
 
-<p><a href="https://stanford.zoom.us/my/KwatMDPhD">Zoom</a> (ask for the passcode).</p>
+<p>share thoughts @ <a href="https://twitter.com/KwatMDPhD">Twitter</a>.</p>
 
-<p>When I complete the things I'm building, I'll share them with you on these channels :)</p>
+<p>meet @ (ask for the passcode) <a href="https://stanford.zoom.us/my/KwatMDPhD">Zoom</a>.</p>
 
 <h3>Every adversity, carries with it, the seed of equivalent advantage.</h3>
 
