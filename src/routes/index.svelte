61392a387aaf9f884ede6a838e89b97383b67de4
KwatMDPhD <kwat.me@icloud.com>
2022-08-12 12:53:31 -0600

    Switched to ColorSchems

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -32,7 +32,7 @@
 
 <h1>Hi, I'm Kwat :)</h1>
 
-<h4>Nomad from Kazakhstan → China → Japan → FIBA U16 Olympian → USA → Leukemia → Homeless → Programmer → Guardiome founder → Stanford MD-PhD candidate → Entrepreneur </h4>
+<h4>Nomad from Kazakhstan → China → Japan → FIBA U16 Olympian → USA → Leukemia → Homeless → Programmer → Guardiome founder → Stanford MD-PhD candidate → (Entrepreneur) </h4>
 
 <h5>These days, I ...</h5>
 
@@ -41,3 +41,5 @@
 {/each}
 
 <h3>Every adversity, carries with it, the seed of equivalent advantage.</h3>
+
+<h3>Become the best in the world first; everything else follows.</h3>
