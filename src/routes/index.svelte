03d2559d2183e50d311c5a24737d7ce62b5661fb
KwatMDPhD <kwat.me@icloud.com>
2022-11-05 18:12:57 -0600

    Peace

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,45 +1,27 @@
 <script>
 
-    let profileLinks = [
-        ["https://github.com/KwatMDPhD", "code", ""],
-        ["https://scholar.google.com/citations?user=PAM4lScAAAAJ&hl=en", "publish", ""],
-        ["https://discord.gg/tKh7fguMrD", "collaborate", "join the bioinformatics community"],
-        ["https://podcast.kwatmdphd.com", "podcast",""],
-        ["https://www.youtube.com/c/Kwat","record", ""],
-        ["https://www.instagram.com/KwatMDPhD", "share moments", ""],
-        ["https://twitter.com/KwatMDPhD","share thoughts",  ""],
-        ["https://stanford.zoom.us/my/KwatMDPhD","meet",  "ask for the passcode"],
+    let links = [
+        ["https://github.com/KwatMDPhD", "code"],
+        ["https://discord.gg/tKh7fguMrD", "collaborate"],
+        ["https://scholar.google.com/citations?user=PAM4lScAAAAJ&hl=en", "publish"],
+        ["https://guardiome.com", "sequence DNA"],
+        ["https://www.amazon.com/s?i=stripbooks&rh=p_27%3AKwat+Medetgul-Ernar&s=relevancerank&text=Kwat+Medetgul-Ernar&ref=dp_byline_sr_book_2", "write books"],
+        ["https://www.mtjiujitsu.com", "teach jiu-jitsu"],
+        ["https://podcast.kwatmdphd.com", "podcast"],
+        ["https://www.youtube.com/c/Kwat", "record"],
+        ["https://www.instagram.com/KwatMDPhD", "share moments"],
+        ["https://twitter.com/KwatMDPhD", "share thoughts"],
+        ["https://stanford.zoom.us/my/KwatMDPhD", "meet"],
     ]
-
-    let productLinks = [
-        ["https://guardiome.com", "Private whole-genome sequencing"],
-        ["https://www.amazon.com/s?i=stripbooks&rh=p_27%3AKwat+Medetgul-Ernar&s=relevancerank&text=Kwat+Medetgul-Ernar&ref=dp_byline_sr_book_2", "Children's book about pathogens"],
-    ]
-
-    function add_suffix(link) {
-
-        let suffix = link[2]
-
-        if (suffix == "") {
-            return "."
-        }
-        else {
-            return " (" + suffix + ")."
-        }
-    }
 
 </script>
 
 <h1>Hi, I'm Kwat :)</h1>
 
-<h4>Nomad from Kazakhstan → China → Japan → FIBA U16 Olympian → USA → Leukemia → Homeless → Programmer → iGEM Gold medalist → Guardiome founder → Stanford MD-PhD candidate → Tiny Beasts founder → Mt Jiu Jitsu founder → </h4>
+<h4>Nomad from Kazakhstan → China → Japan → FIBA U16 Olympian → USA → Leukemia → Homeless → Programmer → iGEM Gold medalist → Guardiome founder → Stanford MD-PhD candidate → Tiny Beasts founder → Mt. Jiu-Jitsu founder → </h4>
 
 <h5>These days, I ...</h5>
 
-{#each profileLinks as link}
-<p><a href={link[0]}>{link[1]}</a>{add_suffix(link)}</p>
+{#each links as link}
+<p><a href={link[0]}>{link[1]}</a>.</p>
 {/each}
-
-<h3>Every adversity, carries with it, the seed of equivalent advantage.</h3>
-
-<h3>Become the best in the world first; everything else follows.</h3>
