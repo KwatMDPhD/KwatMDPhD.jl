e5583b2687692f99b937119647bd691fdb213f39
KwatMDPhD <kwat.me@icloud.com>
2023-04-16 12:40:21 -0600

    Updated links.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,18 @@
 <script>
 
-    let links = [
-        ["https://github.com/KwatMDPhD", "code"],
-        ["https://discord.gg/tKh7fguMrD", "collaborate"],
-        ["https://scholar.google.com/citations?user=PAM4lScAAAAJ&hl=en", "publish"],
-        ["https://guardiome.com", "sequence DNA"],
-        ["https://immunogram.io", "measure the immune system"],
-        ["https://www.amazon.com/s?i=stripbooks&rh=p_27%3AKwat+Medetgul-Ernar&s=relevancerank&text=Kwat+Medetgul-Ernar&ref=dp_byline_sr_book_2", "write books"],
-        ["https://mtjiujitsu.com", "teach jiu-jitsu"],
-        ["https://youtube.com/c/KwatMDPhD", "record video"],
-        ["https://instagram.com/KwatMDPhD", "share moments"],
-        ["https://twitter.com/KwatMDPhD", "share thoughts"],
-        ["https://stanford.zoom.us/my/KwatMDPhD", "meet"],
+    let li_ = [
+        ["https://scholar.google.com/citations?user=PAM4lScAAAAJ&hl=en", "Google Scholar"],
+        ["https://github.com/KwatMDPhD", "GitHub"],
+        ["https://youtube.com/c/KwatMDPhD?sub_confirmation=1", "YouTube"],
+        ["https://", "Podcast"],
+        ["https://twitter.com/KwatMDPhD", "Twitter"],
+        ["https://instagram.com/KwatMDPhD", "Instagram"],
+        ["https://guardiome.com", "Guardiome"],
+        ["https://www.amazon.com/s?i=stripbooks&rh=p_27%3AKwat+Medetgul-Ernar&s=relevancerank&text=Kwat+Medetgul-Ernar&ref=dp_byline_sr_book_2", "Tiny Beasts"],
+        ["https://mountjiujitsu.com", "Mount Jiu-Jitsu"],
+        ["https://immunogram.io", "Immunogram"],
+        ["https://discord.gg/tKh7fguMrD", "Bioinformatics"],
+        ["https://stanford.zoom.us/my/KwatMDPhD", "Zoom"],
     ]
 
 </script>
@@ -20,8 +21,6 @@
 
 <h4>Nomad from Kazakhstan → China → Japan → FIBA U16 Olympian → USA → Leukemia → Homeless → Programmer → iGEM Gold medalist → Guardiome founder → Stanford MD-PhD candidate → Tiny Beasts founder → Chopper Health founder → Mount Jiu-Jitsu founder → Immunogram founder </h4>
 
-<h5>These days, I ...</h5>
-
-{#each links as link}
-<p><a href={link[0]}>{link[1]}</a>.</p>
+{#each li_ as li}
+<p><a href={li[0]}>{li[1]}</a>.</p>
 {/each}
