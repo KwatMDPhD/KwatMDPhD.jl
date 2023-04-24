d813b55dc886075ccb6f00921d744a8d96691583
KwatMDPhD <kwat.me@icloud.com>
2023-04-23 23:33:56 -0600

    Good.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,54 +1,56 @@
 <svelte:head>
   <title>About</title>
 </svelte:head>
-<h1>Hi, I'm Kwat!</h1>
-<br />
-<br />
-<h2>My Journey 🛣️</h2>
-<br />
-<h4>Nomad from Kazakhstan 🏔️👶🐎🍼🇰🇿</h4>
-<p>⬇</p>
-<h4>China 🇨🇳</h4>
-<p>⬇</p>
-<h4>Japan 🇯🇵</h4>
-<p>⬇</p>
-<h4>FIBA U16 Olympian 🌏</h4>
-<p>⬇</p>
-<h4>USA 🇺🇸</h4>
-<p>⬇</p>
-<h4>Leukemia 🤒</h4>
-<p>⬇</p>
-<h4>Homeless 👝</h4>
-<p>⬇</p>
-<h4>Programmer 👨‍💻</h4>
-<p>⬇</p>
-<h4>iGEM Gold Medalist 🏅</h4>
-<p>⬇</p>
-<h4>Stanford MD-PhD Candidate 🌲🥼🩺</h4>
-<p>⬇</p>
-<br />
-<br />
-<h2>My Projects 📈</h2>
-<br />
-<div>
-  <h4>
-    <a href="https://gsea-msigdb.org/gsea/index.jsp"
-      >✨ Gene-Set Enrichment Analysis</a
-    > 🧮
-  </h4>
-  <h4><a href="https://guardiome.com">✨ Guardiome</a> 🧬</h4>
-  <h4><a href="https://immunogram.io">✨ Immunogram</a> 🧫</h4>
-  <h4>
-    <a
-      href="https://www.amazon.com/s?i=stripbooks&rh=p_27%3AKwat+Medetgul-Ernar&s=relevancerank&text=Kwat+Medetgul-Ernar&ref=dp_byline_sr_book_2"
-      >✨ Tiny Beasts</a
-    > 🦠
-  </h4>
-  <h4><a href="https://mountjiujitsu.com">✨ Mount Jiu-Jitsu</a> 🏔️🥋🤼‍♂️</h4>
-  <h4><a href="https://guardiome.com">🤫 Chopper Health</a> 👨‍⚕️</h4>
+<div class="di1">
+  <h1>Hi, I'm Kwat!</h1>
+  <h2>My Journey 🛣️</h2>
+  <h4>Nomad from Kazakhstan 🏔️👶🐎🍼🇰🇿</h4>
+  <p>⬇</p>
+  <h4>China 🇨🇳</h4>
+  <p>⬇</p>
+  <h4>Japan 🇯🇵</h4>
+  <p>⬇</p>
+  <h4>FIBA U16 Olympian 🌏</h4>
+  <p>⬇</p>
+  <h4>USA 🇺🇸</h4>
+  <p>⬇</p>
+  <h4>Leukemia 🤒</h4>
+  <p>⬇</p>
+  <h4>Homeless 👝</h4>
+  <p>⬇</p>
+  <h4>Programmer 👨‍💻</h4>
+  <p>⬇</p>
+  <h4>iGEM Gold Medalist 🏅</h4>
+  <p>⬇</p>
+  <h4>Stanford MD-PhD Candidate 🌲🥼🩺</h4>
+  <p>⬇</p>
+  <h2>My Projects 📈</h2>
+  <div>
+    <h4>
+      <a href="https://gsea-msigdb.org/gsea/index.jsp"
+        >✨ Gene-Set Enrichment Analysis</a
+      > 🧮
+    </h4>
+    <h4><a href="https://guardiome.com">✨ Guardiome</a> 🧬</h4>
+    <h4><a href="https://immunogram.io">✨ Immunogram</a> 🧫</h4>
+    <h4>
+      <a
+        href="https://www.amazon.com/s?i=stripbooks&rh=p_27%3AKwat+Medetgul-Ernar&s=relevancerank&text=Kwat+Medetgul-Ernar&ref=dp_byline_sr_book_2"
+        >✨ Tiny Beasts</a
+      > 🦠
+    </h4>
+    <h4><a href="https://mountjiujitsu.com">✨ Mount Jiu-Jitsu</a> 🏔️🥋🤼‍♂️</h4>
+    <h4><a href="https://guardiome.com">🤫 Chopper Health</a> 👨‍⚕️</h4>
+  </div>
 </div>
 
 <style lang="scss">
+  .di1 {
+    display: flex;
+    gap: $sp2;
+    flex-direction: column;
+  }
+
   * {
     line-height: $sp2;
     text-align: center;
