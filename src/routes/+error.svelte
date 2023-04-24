a04b224cdc58cbf2d661a0e36ed0699c724ad114
KwatMDPhD <kwat.me@icloud.com>
2023-04-24 16:10:05 -0600

    Added +error.svelte.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<h1>{$page.status}</h1>
+<p>{$page.error.message}</p>
+
+<a href="/">Go Back Home</a>
+
+<style lang="scss">
+  a {
+    font-size: $sp3;
+  }
+</style>
