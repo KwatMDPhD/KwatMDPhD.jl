bb80970646bc949ddf2565262064f8cfb3834b7a
KwatMDPhD <kwat.me@icloud.com>
2024-11-14 12:54:46 -0700

    Updates

--- stipple.jl/v0.28.19/assets/js/keepalive.js
+++ stipple.jl/v0.28.19/assets/js/keepalive.js
@@ -1,21 +1,24 @@
 /*
-** keepalive.js // v1.0.0 // 6th January 2022
-** Keeps alive the websocket connection by sending a ping every x seconds
-** where x = Genie.config.webchannels_keepalive_frequency
-*/
+ ** keepalive.js // v1.0.0 // 6th January 2022
+ ** Keeps alive the websocket connection by sending a ping every x seconds
+ ** where x = Genie.config.webchannels_keepalive_frequency
+ */
 
 function keepalive() {
   if (window._lastMessageAt !== undefined) {
-    if (Date.now() - window._lastMessageAt < Genie.Settings.webchannels_keepalive_frequency) {
-      return
+    if (
+      Date.now() - window._lastMessageAt <
+      Genie.Settings.webchannels_keepalive_frequency
+    ) {
+      return;
     }
   }
 
-  if (Genie.Settings.env == 'dev') {
-    console.info('Keeping connection alive');
+  if (Genie.Settings.env == "dev") {
+    console.info("Keeping connection alive");
   }
 
-  Genie.WebChannels.sendMessageTo(CHANNEL, 'keepalive', {
-    'payload': {}
+  Genie.WebChannels.sendMessageTo(CHANNEL, "keepalive", {
+    payload: {},
   });
 }
