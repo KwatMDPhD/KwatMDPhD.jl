76540b621adf0e52cb58a786a0ac5c804a27a5f6
KwatMDPhD <kwat.me@icloud.com>
2024-11-13 20:46:00 -0700

    Updates

--- genieautoreload.jl/v2.2.4/assets/js/autoreload.js
+++ genieautoreload.jl/v2.2.4/assets/js/autoreload.js
@@ -1,12 +1,12 @@
 function autoreload_subscribe() {
-  Genie.WebChannels.sendMessageTo('autoreload', 'subscribe');
-  console.info('Autoreloading ready');
+  Genie.WebChannels.sendMessageTo("autoreload", "subscribe");
+  console.info("Autoreloading ready");
 }
 
 setTimeout(autoreload_subscribe, 2000);
 
-Genie.WebChannels.messageHandlers.push(function(event) {
-  if ( event.data == 'autoreload:full' ) {
+Genie.WebChannels.messageHandlers.push(function (event) {
+  if (event.data == "autoreload:full") {
     location.reload(true);
   }
 });
