6e2e0d1bec61292b6863b13d29c215960336941a
KwatMDPhD <kwat.me@icloud.com>
2024-11-18 16:17:06 -0700

    Make KwatMDPhD.com with GenieStatic.jl.

--- app.jl
+++ app.jl
@@ -2,8 +2,4 @@ using GenieFramework
 
 @genietools
 
-const LA = path"html/layout.html"
-
-@page "/" path"html/index.html" layout = LA
-
-@page "/writing" path"html/writing.html" layout = LA
+@page "/" path"html/index.html" layout = path"html/layout.html"
