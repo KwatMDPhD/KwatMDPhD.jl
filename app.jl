6d342e229de94c2c0a88a4a3ea452d68dfff26d5
KwatMDPhD <kwat.me@icloud.com>
2024-11-18 22:14:20 -0700

    Used julia function to render a component.

--- app.jl
+++ app.jl
@@ -2,4 +2,18 @@ using GenieFramework
 
 @genietools
 
+function h2(st)
+
+    """
+    <h2 class="pb-2 text-2xl font-semibold">$st</h2>"""
+
+end
+
+function link(st, ur)
+
+    """
+    <a target="_blank" href=$ur class="font-medium text-rose-400">$st</a>"""
+
+end
+
 @page "/" path"html/index.html" layout = path"html/layout.html"
