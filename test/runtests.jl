52c968c18529b506c430a4e3e5e62eac9127270b
KwatMDPhD <kwat.me@icloud.com>
2025-01-03 10:52:38 -0700

    Genie Static.

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,28 +14,28 @@ using GenieStatic
 
 const PA = pkgdir(KwatMDPhD)
 
+cd(PA)
+
 # ---- #
 
-cd(PA)
+Genie.Configuration.config!(; log_to_file = false)
+
+# ---- #
 
 Genie.loadapp()
 
 # ---- #
 
+const BA = "http://localhost:8000"
+
 Genie.up()
+
+run(`open $BA --background`)
 
 # ---- #
 
 routes()
 
-# ---- #
-
-const BA = "http://localhost:8000"
-
-# ---- #
-
 String(GenieStatic.get(BA).body)
-
-# ---- #
 
 GenieStatic.make(joinpath(PA, "public"), BA)
